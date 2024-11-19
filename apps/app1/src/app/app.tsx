b469415b4cb2278abef56129349a70bb2413f451
puku0x <puku0x@gmail.com>
2024-11-19 14:19:52 +0900

    Initial commit

---
+++ apps/app1/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="app1" />
+    </div>
+  );
+}
+
+export default App;
